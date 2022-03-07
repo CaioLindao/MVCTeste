b09e4134e2628f04eb642ac9b835b6c42ac839f5
Caio de Almeida Araujo <jubileudasgata@gmail.com>
2022-03-07 15:32:51 -0300

    Changed header layout to be more readable

--- public/js/indexCSS.js
+++ public/js/indexCSS.js
@@ -1,8 +1,8 @@
 $(() => {
   // Muda o tamanho do texto do header para se adaptar ao tamanho da tela
-  const headerWelcome = $("#header-welcome");
-  $(headerWelcome).fitText(0.55);
+  // const headerWelcome = $("#header-welcome");
+  // $(headerWelcome).fitText(0.58);
   // Muda o tamanho do texto do subtitulo para se adaptar ao tamanho da tela
-  const headerSubtitle = $("#header-subtitle");
-  $(headerSubtitle).fitText(1.6);
+  // const headerSubtitle = $("#header-subtitle");
+  // $(headerSubtitle).fitText(1.6);
 });
