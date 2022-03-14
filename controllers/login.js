5746849c82eaf8059020a87c01f72b54bba7aff1
Caio de Almeida Araujo <jubileudasgata@gmail.com>
2022-03-14 14:42:31 -0300

    reboot on admin_login function

--- controllers/login.js
+++ controllers/login.js
@@ -34,49 +34,7 @@ const admin_list = async (req, res) => {
   }
 };
 
-const admin_login = async (req, res) => {
-  let io = req.app.get("socketio");
-  let socketId = req.body.socketId;
-
-  console.log(socketId);
-
-  const login = req.body.login;
-  const password = req.body.password;
-
-  // Validação de inputs
-  if (!(login.length && password.length)) {
-    io.to(socketId).emit("badLogin", { message: "Informe Login e Senha" });
-    res.sendStatus(422);
-  } else {
-    // Caso os inputs estejam corretos, executa a tentativa de login
-    let find = await Admin_model.find();
-
-    try {
-      // Relaciona os dados recebidos com os do database para efetuar o login
-      find.forEach(async (admin) => {
-        let comparison = await bcrypt.compare(password, admin.password);
-        if (comparison) {
-          if (login == admin.login) {
-            // Salva a sessão no database
-            req.session.userid = login;
-            res.sendStatus(200);
-            io.to(socketId).emit("login", {
-              message: "Entrando...",
-              code: 100,
-            });
-          }
-        }
-      });
-      // Caso nenhum corresponda, retorna o resultado ao client
-      io.to(socketId).emit("badLogin", {
-        message: "Login ou Senha incorretos.",
-      });
-    } catch (error) {
-      console.log(error);
-      io.to(socketId).emit("badLogin", { message: "Erro de requisição." });
-    }
-  }
-};
+const admin_login = async (req, res) => {};
 
 const admin_logout = (req, res) => {
   if (req.session.userid) {
